0806f01d07083910cfe97dc111c6e6c9aead5c0d
Sergey <voronin100@gmail.com>
2016-01-03 13:09:49 +0300

    Complete sicp 1.17

---
+++ 1-17.scm
@@ -0,0 +1,15 @@
+#lang planet neil/sicp
+
+(define (double x)
+  (* x 2))
+
+(define (halve x)
+  (/ x 2))
+
+(define (fast-mul a b)
+  (cond ((= b 0) 0)
+        ((even? b) (double (fast-mul a (halve b))))
+        (else
+         (+ a (fast-mul a (- b 1))))))
+
+(fast-mul 3 4)
