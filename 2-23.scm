87a7d5b2e0c21e81f79f55f6afb169080d6b699d
Sergey <voronin100@gmail.com>
2016-04-03 15:35:17 +0300

    Completed sicp 2.23

---
+++ 2-23.scm
@@ -0,0 +1,11 @@
+#lang planet neil/sicp
+
+(define (for-each proc items)
+  (if (null? items)
+      true
+      (begin
+      (proc (car items))
+      (for-each proc (cdr items)))))
+
+(for-each (lambda (x) (newline) (display x))
+          (list 57 321 88))
