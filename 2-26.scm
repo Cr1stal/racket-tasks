4ace51e167dc019e78cda51acfe8a2d4c20f97be
Sergey <voronin100@gmail.com>
2016-04-03 15:35:44 +0300

    Completed sicp 2.26

---
+++ 2-26.scm
@@ -0,0 +1,8 @@
+#lang planet neil/sicp
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(equal? '(1 2 3 4 5 6) (append x y))
+(equal? (cons '(1 2 3) '(4 5 6)) (cons x y))
+(equal? '((1 2 3) (4 5 6)) (list x y))
