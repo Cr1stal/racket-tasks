5bf9fa41fbfb40eb908966cff5aba6348f7c8802
Sergey <voronin100@gmail.com>
2016-04-15 13:29:33 +0300

    Completed sicp 2.38

---
+++ 2-38.scm
@@ -0,0 +1,30 @@
+#lang planet neil/sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (fold-right op initial sequence)
+  (fold-left op initial (reverse sequence)))
+
+(define (reverse sequence)
+  (if (null? sequence)
+      nil
+      (append (reverse (cdr sequence)) (list (car sequence)))))
+
+(fold-right / 1 (list 1 2 3))
+
+(fold-left / 1 (list 1 2 3))
+
+(fold-right list nil (list 1 2 3))
+(fold-left list nil (list 1 2 3))
